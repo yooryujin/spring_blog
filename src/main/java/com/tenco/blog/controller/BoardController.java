d753f2c6d43ecd073849dfe89ac4fc3afbbf5369
CliMinG0 <jhyeonjin365@gmail.com>
2025-06-18 16:36:49 +0900

    기본 뼈대 코드 완성 페이지 연결 처리

--- src/main/java/com/tenco/blog/controller/BoardController.java
+++ src/main/java/com/tenco/blog/controller/BoardController.java
@@ -1,4 +1,40 @@
 package com.tenco.blog.controller;
 
-public class BoardController {  
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller // IoC 대상 - 싱글톤 패턴으로 관리 됨
+public class BoardController {
+
+    @GetMapping({"/","/index"})
+    public String index() {
+        // prefix : /templates/
+        // return : index
+        // suffix : .mustache
+        // # 기본 경로를 src/main/resources/templates/index.mustache
+        return "index";
+    }
+
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        // templates / board
+        // templates / board /
+
+        return "board/save-form";
+    }
+    // board/1
+
+    /**
+     * 게시물 상세보기 요청
+     * board/1
+     */
+   @GetMapping("/board/{id}")
+    public String  detail (@PathVariable (name = "id") int id) {
+        // URL 에서 받은 id 값을 사용해서 특정 게시글 상세보기 조회
+       // 실제로는 이 id 값으로 데이터베이스에 있는 게시글 조회 하고
+       // 머스테치 파일로 데이터를 내려 주어야 함 (Model)
+       return "board/detail";
+   }
+
 }
--- src/main/java/com/tenco/blog/model/Board.java
+++ src/main/java/com/tenco/blog/model/Board.java
@@ -7,15 +7,22 @@ import lombok.Data;
 import java.sql.Timestamp;
 
 @Data
+// @Table : 실제 데이터베이스 테이블 명을 지정할 때 사용
 @Table(name = "board_tb")
+// @Entity : JPA  가 이 클래스를 데이터베이스 테이블과 매핑하는 객체(엔티티)로 인식 한다
+// 즉, @Entity 어노테이션이 있어야 JPA 가 이 객체를 관리 한다
 @Entity
 public class Board {
+
+    // @Id 이 필드가 기본키(Primary key) 임을 나타냄
     @Id
+    // IDENTITY 전략 : 데이터베이스의 기본 전략을 사용한다. -> Auto_Increment
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    // 별도 annotation 이 없으면 필드명이 컬럼명이 됨
     private String title;
     private String content;
     private String username;
-    private Timestamp createdAt;
+    private Timestamp createdAt;  // created_at ( 스네이크 케이스로 자동 변환 )
 }
